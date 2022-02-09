cc891bea09f63806c5dbb5b8eab633b8ceb155b6
Tomasz <tomasz@kulinowski.pl>
2022-02-09 22:08:41 +0100

    add sleep

--- solid.ps1
+++ solid.ps1
@@ -1,3 +1,5 @@
 Get-PnpDevice -FriendlyName "*1050*" | Disable-PnpDevice -confirm:$false
+Start-Sleep -Seconds 1
 Start-Process "D:\Solidworks2018\SOLIDWORKS\SLDWORKS.exe"
+Start-Sleep -Seconds 60
 Get-PnpDevice -FriendlyName "*1050*" | Enable-PnpDevice -confirm:$false
