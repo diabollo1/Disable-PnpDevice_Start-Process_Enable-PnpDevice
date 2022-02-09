9a6a0329444d7db3a9940cad8e480d71f8740112
Tomasz <tomasz@kulinowski.pl>
2022-02-09 22:12:01 +0100

    ensuring opening as administrator

--- solid.ps1
+++ solid.ps1
@@ -1,5 +1,26 @@
+param([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+		Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
+		#Set-ExecutionPolicy RemoteSigned
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+		Set-ExecutionPolicy Restricted
+	}
+    exit
+}
+
 Get-PnpDevice -FriendlyName "*1050*" | Disable-PnpDevice -confirm:$false
 Start-Sleep -Seconds 1
 Start-Process "D:\Solidworks2018\SOLIDWORKS\SLDWORKS.exe"
 Start-Sleep -Seconds 60
 Get-PnpDevice -FriendlyName "*1050*" | Enable-PnpDevice -confirm:$false
+
+exit
